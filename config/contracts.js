91857e83549c54c0520fb5263b10d549be7752a2
smartusdc <jakepaul900009@gmail.com>
2024-12-24 22:07:37 +0900

    Update contracts.js

--- config/contracts.js
+++ config/contracts.js
@@ -1,51 +1,6 @@
 // config/contracts.js
 
 export const CONTRACT_ABI = [
-    // USDC承認
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "spender",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "amount",
-                "type": "uint256"
-            }
-        ],
-        "name": "approve",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    // 残高確認
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "account",
-                "type": "address"
-            }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
     // デポジット
     {
         "inputs": [
@@ -79,70 +34,20 @@ export const CONTRACT_ABI = [
         "stateMutability": "nonpayable",
         "type": "function"
     },
-    // 報酬請求
-    {
-        "inputs": [],
-        "name": "claimDepositReward",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    // リファラル報酬請求
-    {
-        "inputs": [],
-        "name": "claimReferralReward",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    // リファラルコード生成
-    {
-        "inputs": [],
-        "name": "generateReferralCode",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    // ユーザーランク情報取得
+    // デポジット残高照会
     {
         "inputs": [
             {
                 "internalType": "address",
-                "name": "user",
+                "name": "",
                 "type": "address"
             }
         ],
-        "name": "getUserRank",
+        "name": "deposits",
         "outputs": [
             {
                 "internalType": "uint256",
-                "name": "rankId",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "rankName",
-                "type": "string"
-            },
-            {
-                "internalType": "uint256",
-                "name": "bonusRate",
-                "type": "uint256"
-            },
-            {
-                "internalType": "uint256",
-                "name": "nextRankThreshold",
-                "type": "uint256"
-            },
-            {
-                "internalType": "uint256",
-                "name": "progressToNextRank",
+                "name": "",
                 "type": "uint256"
             }
         ],
@@ -172,6 +77,27 @@ export const CONTRACT_ABI = [
 ];
 
 export const USDC_ABI = [
+    // 残高照会
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    // 送金承認
     {
         "inputs": [
             {
@@ -196,6 +122,7 @@ export const USDC_ABI = [
         "stateMutability": "nonpayable",
         "type": "function"
     },
+    // 承認残高照会
     {
         "inputs": [
             {
@@ -210,25 +137,6 @@ export const USDC_ABI = [
             }
         ],
         "name": "allowance",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "account",
-                "type": "address"
-            }
-        ],
-        "name": "balanceOf",
         "outputs": [
             {
                 "internalType": "uint256",
